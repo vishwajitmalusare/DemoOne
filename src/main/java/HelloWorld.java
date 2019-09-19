d7bed411ae604c838df0456bf3daaec34c4dc4c4
VISHWAJEET MALUSARE <vishwajeetmalusare@VISHWAJEETs-MacBook-Pro.local>
2019-09-19 16:32:55 +0530

    [Vishwa] Add. change number two in branch

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World !");
         System.out.println("Changed message in local Repository");
+        System.out.println("Merge to master....");
     }
 }
